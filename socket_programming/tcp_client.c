95d2e53ae43e998c8ab419dd91f63733eaea1fb0
Jessica <21d29.jessica@sjec.ac.in>
2025-04-17 18:13:43 +0530

    socket

--- socket_programming/tcp_client.c
+++ socket_programming/tcp_client.c
@@ -5,53 +5,61 @@
 #include <arpa/inet.h>
 
 #define SERVER_IP "127.0.0.1"
-#define PORT 8080
+#define PORT 1234
 #define BUFFER_SIZE 1024
 
 int main() 
 {
     	int client_socket;
     	struct sockaddr_in server_addr;
-    	char buffer[BUFFER_SIZE];
+    	char buffer[BUFFER_SIZE] = {0};
+	char message[BUFFER_SIZE] = {0};
 
-    	// 1. Create the socket
-    	client_socket = socket(AF_INET, SOCK_STREAM, 0);
-    	if (client_socket < 0) 
-    	{
-        	perror("Socket creation failed");
-        	exit(EXIT_FAILURE);
-    	}
-
-    	// 2. Set up the server address structure
-    	memset(&server_addr, 0, sizeof(server_addr));
-    	server_addr.sin_family = AF_INET;
-    	server_addr.sin_port = htons(PORT);
-    	if (inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr) <= 0) 
-    	{
-        	perror("Invalid address");
-        	close(client_socket);
-        	exit(EXIT_FAILURE);
-    	}
+    	while(1)
+	{
+		// 1. Create the socket
+    		client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    		if (client_socket < 0) 
+    		{
+        		perror("Socket creation failed");
+        		exit(EXIT_FAILURE);
+    		}
 	
-    	// 3. Connect to the server
-    	if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) 
-    	{
-        	perror("Connection failed");
-        	close(client_socket);
-        	exit(EXIT_FAILURE);
-    	}
+    		// 2. Set up the server address structure
+    		memset(&server_addr, 0, sizeof(server_addr));
+    		server_addr.sin_family = AF_INET;
+    		server_addr.sin_port = htons(PORT);
+    	
+		if (inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr) <= 0) 
+    		{
+        		perror("Invalid address");
+        		close(client_socket);
+        		exit(EXIT_FAILURE);
+    		}
+	
+    		// 3. Connect to the server
+    		if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) 
+    		{
+        		perror("Connection failed");
+        		close(client_socket);
+        		exit(EXIT_FAILURE);
+    		}
 
-    	// 4. Send data to the server
-    	strcpy(buffer, "Hello from the client!");
-    	send(client_socket, buffer, strlen(buffer), 0);
+    		// 4. Send data to the server
+    		printf("Enter message to send to server: ");
+    		fgets(message, sizeof(message), stdin);
+    		message[strcspn(message, "\n")] = '\0';
+    	
+		send(client_socket, message, strlen(message), 0);
+	
+    		// 5. Receive a message from the server
+    		memset(buffer, 0, BUFFER_SIZE);
+    		recv(client_socket, buffer, BUFFER_SIZE, 0);
+    		printf("Received from server: %s\n", buffer);
 
-    	// 5. Receive a message from the server
-    	memset(buffer, 0, BUFFER_SIZE);
-    	recv(client_socket, buffer, BUFFER_SIZE, 0);
-    	printf("Received from server: %s\n", buffer);
-
-    	// 6. Close the socket
-    	close(client_socket);
+    		// 6. Close the socket
+    		close(client_socket);
+	}
 
     	return 0;
 }
--- socket_programming/tcp_server.c
+++ socket_programming/tcp_server.c
@@ -11,6 +11,7 @@ int main()
 {
     	int server_socket; 
 	int client_socket;
+	int opt = 1;
     	struct sockaddr_in server_addr, client_addr;
     	char buffer[BUFFER_SIZE];
     	socklen_t client_len = sizeof(client_addr);
@@ -24,10 +25,12 @@ int main()
     	}
 
     	// 2. Set up the server address structure
-    	memset(&server_addr, 0, sizeof(server_addr));
+    	memset(&server_addr, 0, sizeof(server_addr));  //clears the server_addr memory before filling 
     	server_addr.sin_family = AF_INET;
-    	server_addr.sin_addr.s_addr = INADDR_ANY;
-    	server_addr.sin_port = htons(PORT);
+    	server_addr.sin_addr.s_addr = INADDR_ANY;  //accepts connections from any IP address
+    	server_addr.sin_port = htons(PORT);//convert to network byte order(big endian) coz both ends should use same format(little/big endian)
+
+	setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)); //to reuse the same socket
 
     	// 3. Bind the socket to the IP and port
     	if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) 
@@ -47,28 +50,30 @@ int main()
 	
     	printf("Server is listening on port %d...\n", PORT);
 
-    	// 5. Accept a connection from a client
-    	client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_len);
-    	if (client_socket < 0) 
-    	{
-        	perror("Server accept failed");
-        	close(server_socket);
-        	exit(EXIT_FAILURE);
-	}
+	while(1)
+	{
+    		// 5. Accept a connection from a client
+    		client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_len);
+    		if (client_socket < 0) 
+    		{
+        		perror("Server accept failed");
+        		close(server_socket);
+        		exit(EXIT_FAILURE);
+		}
 	
-    	printf("Client connected\n");
+    		printf("Client connected\n");
 
-    	// 6. Receive and send data
-    	memset(buffer, 0, BUFFER_SIZE);
-    	recv(client_socket, buffer, BUFFER_SIZE, 0);
-    	printf("Received from client: %s\n", buffer);
+    		// 6. Receive and send data
+    		memset(buffer, 0, BUFFER_SIZE);
+    		recv(client_socket, buffer, BUFFER_SIZE, 0);
+    		printf("Received from client: %s\n", buffer);
 
-    	// Send a message to the client
-    	strcpy(buffer, "Hello from the server!");
-    	send(client_socket, buffer, strlen(buffer), 0);
+    		// Send a message to the client
+    		send(client_socket, buffer, strlen(buffer), 0);
 
-    	// 7. Close the sockets
-    	close(client_socket);
+    		// 7. Close the sockets
+    		close(client_socket);
+	}	
     	close(server_socket);
 
     	return 0;
